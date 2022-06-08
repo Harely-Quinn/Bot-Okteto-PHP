97ce152d2fb874ad07f964fd68dc3932942865f2
Harely Quinn <72539044+Harely-Quinn@users.noreply.github.com>
2022-06-08 15:23:23 +0300

    Create boot.php

---
+++ boot.php
@@ -0,0 +1,60 @@
+<?php
+ob_start();
+define('API_KEY','5539182722:AAFF68E0X8OomkYd5hPM3EEEABoqSFoEugY');
+//type the token
+$admin = "1188806296"; 
+//Enter your id number
+function bot($method,$datas=[]){
+    $url = "https://api.telegram.org/bot".API_KEY."/".$method;
+    $ch = curl_init();
+    curl_setopt($ch,CURLOPT_URL,$url);
+    curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
+    curl_setopt($ch,CURLOPT_POSTFIELDS,$datas);
+    $res = curl_exec($ch);
+    if(curl_error($ch)){
+        var_dump(curl_error($ch));
+    }else{
+        return json_decode($res);
+    }
+}
+
+
+$update = json_decode(file_get_contents('php://input'));
+$message = $update->message;
+$chat_id = $message->chat->id;
+$user_id = $message->from->id;
+$message_id = $message->message_id;
+$text = $message->text;
+$cty = $message->chat->type;
+$message->from->first_name;
+$user = $message->from->username;
+$id = $message->from->id;
+$reply= $message->reply_to_message->text;
+$replyid = $message->reply_to_message->from->id;
+$replyname = $message->reply_to_message->from->first_name;
+$title = $message->chat->title;
+
+$call = $update->callback_query;
+$mes = $call->message;
+$data = $call->data;
+$qid = $call->id;
+$callcid = $mes->chat->id;
+$callmid = $mes->message_id;
+$callfrid = $call->from->id;
+$calluser = $mes->chat->username;
+$callfname = $call->from->first_name;
+
+$new = $message->new_chat_member;
+$new_id = $new->id;
+$new_name = $new->first_name;
+
+
+
+
+if($text == "/start"){
+ bot('sendMessage',[
+ 'chat_id'=>$chat_id,
+ "text"=>"test"
+ ]);
+ }
+?>
